7135f48cd4134b725b77af29dd9fe96c3f072576
aseelflihan <aseelflihan@gmail.com>
2023-01-09 20:16:56 +0300

    complated assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+function writeCards(names, gift) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+  }
+  
+  function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+
+  
